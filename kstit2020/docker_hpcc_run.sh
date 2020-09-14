986f59b7406f09866a1dfb0a3af8227e622e2d56
PetrerW <piotrw76@gmail.com>
2020-09-15 01:40:48 +0200

    Cleanup, add Dockerfile and docker script

--- kstit2020/docker_hpcc_run.sh
+++ kstit2020/docker_hpcc_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run -v $(pwd)/hpcc-kstit2020/:/mnt/kstit2020/ --name hpcc-kstit -it ubuntu-hpcc /bin/bash
+docker run -v $(pwd)/hpcc-kstit2020/:/mnt/kstit2020/ --name hpcc-kstit -it petrerw/ubuntu-hpcc:2.0 /bin/bash
 #/mnt/kstit2020/run_display_hpcc
 # docker exec -it hpcc-kstit /mnt/kstit2020/run_hpcc
 #docker exec hpcc-kstit cat /mnt/kstit2020/hpcc10.txt
---
+++ kstit2020/hpcc-kstit2020/run_hpcc.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+ 
+#simulate
+rm hpcc10.txt
+touch hpcc10.txt
+
+for i in  1 2 3 4 5 6 7 8 9 10
+do
+ echo "Run number $i"
+ mpirun -np 20 ./hpcc 
+ cat hpccoutf.txt >> hpcc10.txt
+ sleep 10 
+done 
+
+
+
